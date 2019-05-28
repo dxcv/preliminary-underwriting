01e0a3e1a74bb8676599ac411b8fe7239e371dcf
lujunhui <lujunhui_c@163.com>
2019-05-28 15:53:49 +0800

    login样式

--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -8,3 +8,23 @@ function showModal(){
     $('.add_modal').addClass('tran_scale');
     $('.shadow').show();
 }
+
+$('.login').on('click',function () {
+    var name=$('.name').val();
+    var password=$('.password').val();
+    if(name != '' && password !=''){
+        $.ajax({
+            url: "/administrator/login",
+            type : 'get',
+            data: {
+                'userName':name,
+                'password':password
+            },
+            dataType: "JSON",
+            success: function (data) {
+                console.log(data);
+                window.location.href='/admin/underwriting/select';
+            }
+        })
+    }
+})
